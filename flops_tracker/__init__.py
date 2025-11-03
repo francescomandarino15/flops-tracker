d480905ac5155367afc3f1642f1bced6b372e9b4
francescomandarino15 <francescomandarino81@gmail.com>
2025-11-03 21:35:21 +0100

    Update __init__.py

--- flops_tracker/__init__.py
+++ flops_tracker/__init__.py
@@ -1,3 +1,4 @@
 from .tracker import FlopsTracker
-from .estimators import SklearnSGDEstimator, TorchCNNLayerwiseEstimator
-__all__ = ["FlopsTracker", "SklearnSGDEstimator", "TorchCNNLayerwiseEstimator"]
+from . import estimators
+
+__all__ = ["FlopsTracker", "estimators"]
